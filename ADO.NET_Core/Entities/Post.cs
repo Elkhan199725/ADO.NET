9e1bf2b064091f8cca2776da5aca944bc488b519
Elkhan Mammadli <elkhantma@code.edu.az>
2024-01-23 22:26:50 +0400

    added data

---
+++ ADO.NET_Core/Entities/Post.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NET_Core.Entities;
+
+public class Post
+{
+    public int UserId { get; set; }
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Body { get; set; }
+    public Post(int userId, int id, string title, string body)
+    {
+        UserId = userId;
+        Id = id;
+        Title = title;
+        Body = body;
+    }
+}
